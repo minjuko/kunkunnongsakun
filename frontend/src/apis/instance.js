259559c7924fb38af6baa5e44bd2840ab5747c67
minjuko <rhalswn0526@gmail.com>
2026-08-15 17:46:51 +0900

    chore: 환경변수 템플릿과 ignore 규칙 추가

--- frontend/src/apis/instance.js
+++ frontend/src/apis/instance.js
@@ -1,7 +1,8 @@
 // instance.js
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:8000";
 export const instance = axios.create({
   baseURL: BASE_URL,
   timeout: 50000,
