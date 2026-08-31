e735c01a191bf438967fadd2eb6ab1b2927c83cf
minjuko <rhalswn0526@gmail.com>
2026-08-31 10:34:18 +0900

    chore: 미사용 패키지 제거

--- frontend/src/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  resolve: {
-    fallback: {
-      https: require.resolve("https-browserify"),
-      stream: require.resolve("stream-browserify"),
-      assert: require.resolve("assert/"),
-    },
-  },
-};
